56b11d5d7404318678eff83fc4db615ca0e9c280
Kedar Sarmalkar <kedar.kesar@gmail.com>
2014-06-12 22:51:24 -0400

    Moved to gradle

--- sample/src/main/java/com/viewpagerindicator/sample/ListSamples.java
+++ sample/src/main/java/com/viewpagerindicator/sample/ListSamples.java
@@ -1,12 +1,5 @@
 package com.viewpagerindicator.sample;
 
-import java.text.Collator;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -15,6 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ListSamples extends ListActivity {
 
